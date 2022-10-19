42c6fb096bfb8dd6f2cc49f0ac26e94c5a09008f
zhengpuas47 <zhengpuas47@gmail.com>
2022-10-19 17:14:49 -0400

    bug fixed in re

--- Data_Storage/check_archives.py
+++ Data_Storage/check_archives.py
@@ -40,7 +40,7 @@ if __name__ == "__main__":
         _full_file = os.path.join(target_folder, _file)
         if os.path.isfile(_full_file):
             # check if its a filelist item
-            re_string = r'filelist_(?P<fov>[0-9]+)\.txt$)'
+            re_string = r'filelist_(?P<fov>[0-9]+)\.txt$'
             _match_result = re.match(re_string, _file)
             if _match_result:
                 _fov = _match_result.groupdict()['fov']
